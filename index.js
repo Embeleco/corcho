674583b4e016f72c9dbb94963e50c15b4390445e
Izan Tato Izquierdo <103002038+Embeleco@users.noreply.github.com>
2026-09-06 15:38:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
                 // Tamaño del lienzo del corcho
-        const SUPABASE_URL = 'https://pwisexqdcresgevbgkaz.supabase.co'; 
-        const SUPABASE_ANON_KEY = 'sb_publishable_OgJFosGU24LRz3f6EBBDpw_mmAQBR9p'; 
-        const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+        // const SUPABASE_URL = 'https://pwisexqdcresgevbgkaz.supabase.co'; 
+        // const SUPABASE_ANON_KEY = 'sb_publishable_OgJFosGU24LRz3f6EBBDpw_mmAQBR9p'; 
+        // const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
         const ANCHO_CORCHO = 5000;
         const ALTO_CORCHO = 5000;
-        const HORAS_DURACION = 48;
+        // const HORAS_DURACION = 48;
         // 1. Centrar la pantalla al cargar la página por primera vez
         window.onload = function() {
             const centroX = (ANCHO_CORCHO - window.innerWidth) / 2;
